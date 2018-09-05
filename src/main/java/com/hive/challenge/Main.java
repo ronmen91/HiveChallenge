6c7843fe0e72d41d6315953fed8ee9c6262b9e30
RolandNemeth <Roland_Nemeth@epam.com>
2018-09-05 19:48:25 +0200

    Main entry point has been finalized

--- src/main/java/com/hive/challenge/Main.java
+++ src/main/java/com/hive/challenge/Main.java
@@ -1,10 +1,15 @@
 package com.hive.challenge;
 
+import com.hive.challenge.domain.Occurrence;
+import com.hive.challenge.service.FileProcessor;
+import com.hive.challenge.service.ProcessorStrategy;
 import com.hive.challenge.service.impl.WordFileProcessor;
 import com.hive.challenge.service.impl.WordProcessorStrategy;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,7 +27,13 @@ public class Main {
     try {
       Path inputPath = Paths.get(args[0]);
       Path outputPath = Paths.get(args[1]);
-      
+
+      FileProcessor<Map<String, Occurrence>> fileProcessor = new WordFileProcessor(inputPath, outputPath);
+      ProcessorStrategy<Map<String, Occurrence>> processorStrategy = new WordProcessorStrategy();
+
+      Map<String, Occurrence> words = fileProcessor.readInputFile();
+      List<String> processedWords = processorStrategy.process(words);
+      fileProcessor.writeToOutputFile(processedWords);
     } catch (InvalidPathException e) {
       logger.error(INVALID_PATH);
     }
